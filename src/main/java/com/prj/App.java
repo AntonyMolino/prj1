9bdfc2b520f66b3a484d113216301c937bea9a85
lai2-01 <lai2-01@itismeucci.com>
2023-09-18 11:17:50 +0200

    prova

--- src/main/java/com/prj/App.java
+++ src/main/java/com/prj/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args )
     {
         String a;
-        a = ciao;
+        a = "ciao";
         a = null;
         
         System.out.println( "Hello World!" );

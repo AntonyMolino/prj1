97c6f47c612b31feb323d49fe635d8058dcb551f
lai2-01 <lai2-01@itismeucci.com>
2023-09-18 11:12:42 +0200

    bella

---
+++ src/main/java/com/prj/App.java
@@ -0,0 +1,18 @@
+package com.prj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+    public static void main( String[] args )
+    {
+        String a;
+        a = ciao;
+        a = null;
+        
+        System.out.println( "Hello World!" );
+    }
+}
